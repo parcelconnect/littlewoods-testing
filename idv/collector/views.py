3b1f0fd384cdc3f9df958b74b7cd2c6f91f405fb
sawidis <themistoklissavvidis@scurri.com>
2016-04-26 12:14:15 +0300

    Refactor collector:collect view

--- idv/collector/views.py
+++ idv/collector/views.py
@@ -1,6 +1,19 @@
-from django.views.generic import TemplateView
+import json
+
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.views.generic import View
 
 
-class Collect(TemplateView):
+class Collect(View):
 
-    template_name = 'collector/collect.html'
+    def get(self, request):
+        context = {}
+        context['json_context'] = json.dumps({
+            'sign_s3_request_url': reverse('collector:sign-s3-request')
+        })
+        return render(request, 'collector/collect.html', context=context)
+
+    def post(self, request):
+        from django.http import HttpResponseRedirect
+        return HttpResponseRedirect(reverse('collector:collect'))
