65452ef64a9406fc8f3597f0be92361be8ccf206
sawidis <themistoklissavvidis@scurri.com>
2016-04-27 15:09:52 +0300

    Add collector app models for tracking uploads
    - Add models: Account, Credential
    - Contains migrations

--- idv/collector/models.py
+++ idv/collector/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+
+    email = models.EmailField()
+    account_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('email', 'account_number')
+
+
+class Credential(models.Model):
+
+    original_filename = models.CharField(max_length=256, null=False)
+    s3_key = models.CharField(max_length=5, null=False)
+    upload_confirmed = models.BooleanField(default=False)
+    account = models.ForeignKey(Account)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True)
