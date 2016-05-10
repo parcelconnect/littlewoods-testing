27a953c41a0eb09ff3777b5bf2a1bac71ba556c8
sawidis <themistoklissavvidis@scurri.com>
2016-05-10 12:16:16 +0300

    Add settings utility for retrieving a list from env

--- idv/settings/utils.py
+++ idv/settings/utils.py
@@ -2,9 +2,7 @@ import os
 
 
 def env_as_bool(var, default=False):
-    """
-    Read the variable from the environment as a bool.
-    """
+    """Read the variable from the environment as a bool."""
     if var not in os.environ:
         return default
 
@@ -13,3 +11,11 @@ def env_as_bool(var, default=False):
     except KeyError:
         return default
     return value in ['true', '1', 'yes']
+
+
+def env_as_list(var, default=[]):
+    """Read the variable from the environment as a a list."""
+    value = os.getenv(var, '')
+    if not value:
+        return default
+    return value.split()
