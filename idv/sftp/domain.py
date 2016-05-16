2882334044053e32ab224ae59e25f651dbda9348
sawidis <themistoklissavvidis@scurri.com>
2016-05-16 14:51:21 +0300

    Add sftp support for http proxies

--- idv/sftp/domain.py
+++ idv/sftp/domain.py
@@ -4,72 +4,73 @@ import logging
 
 import paramiko
 
+from .proxy import http as http_proxy_util
+
 logger = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
-def get_sftp_client_from_model(cfg, create_host_key=True):
-    client = _get_sftp_client_from_model(cfg, create_host_key=create_host_key)
+def get_sftp_client_from_model(sftp_account, http_proxy):
+    client = _get_sftp_client_from_model(sftp_account, http_proxy)
     yield client
     client.sock.transport.close()
 
 
-def _get_sftp_client_from_model(cfg, create_host_key=True):
-    """Instantiate SFTP client from a SftpAccount model.
-
-    Args:
-        create_host_key: if True (the default) the host key will be
-            stored if this was the first time connecting to the host;
-            otherwise, an exception will be raised.
+def _get_sftp_client_from_model(sftp_account, http_proxy=None):
+    """
+    Instantiate SFTP client from a SftpAccount model.
 
     Returns:
         paramiko.SFTPClient: connected to the host
     """
-    transport = paramiko.Transport((cfg.host, cfg.port))
+    sock = get_transport_sock(sftp_account, http_proxy)
+    transport = paramiko.Transport(sock)
     transport.start_client()
 
     server_key = transport.get_remote_server_key()
+    if not sftp_account.host_key_data:
+        store_host_key_data(sftp_account, server_key)
+        logger.info('Stored key data for for host %s.', sftp_account.host)
+    else:
+        ensure_host_key_matches(sftp_account, server_key)
 
+    transport.auth_password(sftp_account.username, sftp_account.password)
+    return paramiko.SFTPClient.from_transport(transport)
+
+
+def get_transport_sock(sftp_account, http_proxy=None):
+    if http_proxy:
+        host, port = sftp_account.host, sftp_account.port
+        return http_proxy_util.get_tunneled_sock(host, port, http_proxy)
+    else:
+        return (sftp_account.host, sftp_account.port)
+
+
+def store_host_key_data(sftp_account, server_key):
+    key_type = server_key.get_name()
+    sftp_account.host_key_type = key_type
+    sftp_account.host_key_data = server_key.get_base64()
+    sftp_account.save()
+
+
+def ensure_host_key_matches(sftp_account, server_key):
     key_type = server_key.get_name()
     key_data = base64.b64decode(server_key.get_base64())
     key_fprint = get_key_fingerprint(server_key)
 
-    if not cfg.host_key_data:
-        if not create_host_key:
-            raise SftpClientException(
-                'No host key specified for host {host}, yet '
-                'create_host_key=False was specified. '
-                'Provided key is {key_type}: {key_fingerprint}'
-                .format(host=cfg.host,
-                        key_type=key_type,
-                        key_fingerprint=key_fprint))
-
-        logger.info(
-            'This is the first time we connect to %(host)s. '
-            'Storing %(key_type)s key %(key_fingerprint)s.',
-            dict(host=cfg.host, key_type=key_type, key_fingerprint=key_fprint))
-
-        cfg.host_key_type = key_type
-        cfg.host_key_data = server_key.get_base64()
-        cfg.save()
-
-    else:
-        # Ensure that the host key matches
-        if (cfg.host_key_type != key_type or
-                cfg.host_key_data_binary != key_data):
-
-            raise SftpClientException(
-                'WARNING: Remote host identification has changed! Host {host} '
-                'provided {new_key_type} key: {new_key_fingerprint}, '
-                'expected {old_key_type} key: {old_key_fingerprint}'
-                .format(host=cfg.host,
-                        new_key_type=key_type,
-                        new_key_fingerprint=key_fprint,
-                        old_key_type=cfg.host_key_type,
-                        old_key_fingerprint=cfg.host_key_fingerprint))
-
-    transport.auth_password(cfg.username, cfg.password)
-    return paramiko.SFTPClient.from_transport(transport)
+    if (
+        sftp_account.host_key_type != key_type or
+        sftp_account.host_key_data_binary != key_data
+    ):
+        raise SftpClientException(
+            'WARNING: Remote host identification has changed! Host {host} '
+            'provided {new_key_type} key: {new_key_fingerprint}, '
+            'expected {old_key_type} key: {old_key_fingerprint}'
+            .format(host=sftp_account.host,
+                    new_key_type=key_type,
+                    new_key_fingerprint=key_fprint,
+                    old_key_type=sftp_account.host_key_type,
+                    old_key_fingerprint=sftp_account.host_key_fingerprint))
 
 
 class SftpClientException(Exception):
---
+++ idv/sftp/proxy/http.py
@@ -0,0 +1,30 @@
+import base64
+from collections import namedtuple
+from http.client import HTTPConnection
+
+
+HttpProxy = namedtuple('HttpProxy', ['host', 'port', 'username', 'password'])
+
+
+def get_tunneled_connection(host, port, proxy):
+    headers = {}
+    if proxy.username and proxy.password:
+        headers['Proxy-Authorization'] = get_proxy_auth_header(proxy)
+
+    connection = HTTPConnection(proxy.host, proxy.port)
+    connection.set_tunnel(host, port, headers)
+    connection.connect()
+
+    return connection
+
+
+def get_proxy_auth_header(proxy):
+    auth = '{}:{}'.format(proxy.username, proxy.password)
+    base64auth = base64.b64encode(auth.encode('ascii')).decode()
+    proxy_authorization_header = 'Basic ' + base64auth
+    return proxy_authorization_header
+
+
+def get_tunneled_sock(host, port, proxy):
+    connection = get_tunneled_connection(host, port, proxy)
+    return connection.sock
