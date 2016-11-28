323cc337cf0f26e81ccfdab1c8ca6dad9c8c4b06
sawidis <themistoklissavvidis@scurri.com>
2016-11-28 15:26:32 +0000

    Initialize gift wrapping app

--- idv/urls.py
+++ idv/urls.py
@@ -5,5 +5,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^IDV/', include('idv.collector.urls', namespace='collector')),
-    url('^track/', include('idv.tracker.urls', namespace='tracker'))
+    url('^track/', include('idv.tracker.urls', namespace='tracker')),
+    url('^gift-wrapping/', include('idv.giftwrap.urls', namespace='giftwrap')),
 ]
