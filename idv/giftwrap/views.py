7907a99e165074c2677e0d131ecf3c12e3432a9e
sawidis <themistoklissavvidis@scurri.com>
2016-11-29 16:06:00 +0000

    Convert RequestWrap to FormView

--- idv/giftwrap/views.py
+++ idv/giftwrap/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+
+from .forms import GiftWrapRequestForm
 
 
-class RequestWrap(TemplateView):
-
+class RequestWrap(FormView):
     template_name = 'giftwrap/request_wrap.html'
+    form_class = GiftWrapRequestForm
