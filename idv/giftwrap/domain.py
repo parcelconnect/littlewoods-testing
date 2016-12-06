3227c8c0bceb62d61a3e7442302710b3faf5d1ae
Miquel Torres <miqueltorres@scurri.com>
2016-12-06 12:07:51 +0000

    Consistently name variables gift_wrap_request

--- idv/giftwrap/domain.py
+++ idv/giftwrap/domain.py
@@ -1,14 +1,14 @@
 from .ifs import get_client_from_settings
 
 
-def request_gift_wrap(giftwrap_request):
+def request_gift_wrap(gift_wrap_request):
     client = get_client_from_settings()
-    if giftwrap_request.divert_address:
+    if gift_wrap_request.divert_address:
         address = {
-            'address': giftwrap_request.divert_address,
-            'name': giftwrap_request.divert_contact_name,
-            'phone_number': giftwrap_request.divert_contact_number,
+            'address': gift_wrap_request.divert_address,
+            'name': gift_wrap_request.divert_contact_name,
+            'phone_number': gift_wrap_request.divert_contact_number,
         }
     else:
         address = None
-    client.request_gift_wrap(giftwrap_request.upi, address)
+    client.request_gift_wrap(gift_wrap_request.upi, address)
