9d88862a85554c0dcd4c3786f940d0d5ade4b11b
graciek <gracekelly@scurri.com>
2016-12-07 16:18:00 +0000

    installed raven??

---
+++ idv/settings/logs.py
@@ -0,0 +1,40 @@
+import os
+
+from .django import INSTALLED_APPS
+
+LOG_HANDLERS = ['console']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        }
+    }
+}
+
+RAVEN_CONFIG = {'dsn': os.environ.get('RAVEN_DSN')}
+
+if RAVEN_CONFIG['dsn']:
+    INSTALLED_APPS += [
+        'raven.contrib.django.raven_compat',
+    ]
+    LOGGING['handlers']['sentry'] = {
+        'level': 'ERROR',
+        'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+    }
+
+LOGGING['loggers'] = {
+    'django': {
+        'handlers': LOG_HANDLERS,
+        'level': 'INFO',
+        'propagate': False,
+    },
+    'fwtracking.tracking': {
+        'handlers': LOG_HANDLERS,
+        'level': 'INFO',
+        'propagate': False,
+    },
+}
