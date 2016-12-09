1f78c7b3b4f14cce118012b02c2bbaa6918e3b89
Miquel Torres <miqueltorres@scurri.com>
2016-12-09 16:22:57 +0000

    Remove unused internal app

--- idv/internal/mixins.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-
-
-class StaffRequiredMixin(UserPassesTestMixin):
-
-    def test_func(self):
-        user = self.request.user
-        return user.is_authenticated() and (user.is_staff or user.is_admin)
