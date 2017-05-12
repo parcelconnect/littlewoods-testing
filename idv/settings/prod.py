2adfcda187fec26e44cf13c5db1d501364b0375b
Miquel Torres <miqueltorres@scurri.com>
2017-05-12 16:24:00 +0100

    Set https header so redirect works

--- idv/settings/prod.py
+++ idv/settings/prod.py
@@ -5,3 +5,4 @@ WSGI_APPLICATION = 'idv.wsgi.application'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
