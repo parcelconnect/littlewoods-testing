ea8a6df0bf8d8deb1b0536dedd42d28cf4ffcda5
Miquel Torres <miqueltorres@scurri.com>
2017-09-15 17:05:00 +0100

    Add info logging on start and end of move command

--- idv/mover/management/commands/move.py
+++ idv/mover/management/commands/move.py
@@ -1,7 +1,11 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from idv.common.decorators import retry
 from idv.mover import commands as mover_commands
+
+logger = logging.getLogger(__name__)
 
 
 @retry(tries=5, delay=60)
@@ -12,4 +16,6 @@ def move():
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        logger.info('Starting moving credential files...')
         move()
+        logger.info('Finished moving credential files.')
