950e5fa736948ccd983624ad237ecaecadc162c6
Brian Hernandez <brianhernandez@scurri.com>
2017-12-07 14:25:12 +0000

    Fixed the message as I put it in the wrong piece of code

--- idv/common/fastway/client.py
+++ idv/common/fastway/client.py
@@ -124,13 +124,13 @@ class Client:
             response = self._get_json('tracktrace/detail/{}'.format(label_id))
         except FastwayAPIError as e:
             if 'Label' in str(e) and 'does not have any scans' in str(e):
-                raise LabelNotFound('Label ID not found')
-            elif 'Invalid label number' in str(e):
-                raise ValidationError(
+                raise LabelNotFound(
                     'No tracking information found for {}.<br />This means '
                     'your order has not yet been processed and shipped.<br />'
                     'Please contact Littlewoods Customer Service for a status '
                     'update.'.format(label_id))
+            elif 'Invalid label number' in str(e):
+                raise ValidationError('Invalid label ID')
             else:
                 raise
         data = []
