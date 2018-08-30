9dfca21881846a46563d4a20862333569659c8bb
Brian Hernandez <brianhernandez@scurri.com>
2018-08-30 15:19:44 +0100

    Fixed files validation by removing the required attribute Fixed variable names and some methods renamed and separated

--- idv/static/js/upload.js
+++ idv/static/js/upload.js
@@ -3,8 +3,6 @@
 var IDV = window.IDV || {};
 var Django = window.Django || {};
 
-(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
-
 function readFileAsBinaryString(file) {
   return new Promise((resolve, reject) => {
     const fr = new FileReader();
@@ -227,7 +225,7 @@ IDV.UploadForm = (function() {
   let $form = null;
   let progressBars = null;
   let useMultiple = true;
-  let fileInputs = 1;
+  let fileInputsCount = 1;
 
   function createImageThumbnail(picFile, name) {
     const div = document.createElement("div");
@@ -246,14 +244,15 @@ IDV.UploadForm = (function() {
       return;
     }
 
-    filesInput.unbind('change');
+    filesInput.off('change');
 
     filesInput.on("change", function(event) {
       output.empty();
       let imgCount = 0;
 
       if (useMultiple) {
-        imgCount = appendThumbnail(event.target.files, output);
+        drawThumbnails(event.target.files, output);
+        imgCount = event.target.files.length;
       } else {
         $('label[for=' + $(this).attr('id') + ']').find('span').html(event.target.files[0].name);
         let files = [];
@@ -264,7 +263,8 @@ IDV.UploadForm = (function() {
             }
           }
         });
-        imgCount += appendThumbnail(files, output);
+        drawThumbnails(files, output);
+        imgCount = files.length;
       }
 
       const infoText = document.createElement("p");
@@ -274,14 +274,12 @@ IDV.UploadForm = (function() {
     });
   }
 
-  function appendThumbnail(files, output) {
-    let imgCount = 0
+  function drawThumbnails(files, output) {
     for(let i = 0; i < files.length; i++) {
       const file = files[i];
       if(!file.type.match("image"))
           continue;
 
-      imgCount++;
       const picReader = new FileReader();
       $(picReader).on("load", function(event) {
           const div = createImageThumbnail(event.target, file.name)
@@ -289,7 +287,6 @@ IDV.UploadForm = (function() {
       });
       picReader.readAsDataURL(file);
     }
-    return imgCount;
   }
 
   function showUploadSuccessMessage() {
@@ -327,7 +324,9 @@ IDV.UploadForm = (function() {
     }
     let files = [];
     $.each($fileInput, function(index, value){
-      files.push(value.files[0]);
+      if (typeof value.files[0] !== 'undefined') {
+        files.push(value.files[0]);
+      }
     })
     return files;
   }
@@ -336,6 +335,10 @@ IDV.UploadForm = (function() {
     event.preventDefault();
 
     const files = getFormFiles();
+    if (files.length == 0) {
+      showFilesRequiredMessage();
+      return;
+    }
     progressBars.reset();
     progressBars.addMany(files);
 
@@ -346,6 +349,13 @@ IDV.UploadForm = (function() {
     IDV.FormUtils.clearErrors();
 
     idv_form.send(progressBars).then(showUploadSuccessMessage).catch(failedSignHandler);
+  }
+
+  function showFilesRequiredMessage() {
+    let content = '<div class="row" style="margin: 2vh auto 2vh auto"><div class="col-md-12">\
+        Select at least one image, please</div><div>';
+    $('#js-modal .modal-content').html(content);
+    $('#js-modal').modal('show');
   }
 
   function showUploadNotSupportedMessage() {
@@ -359,6 +369,13 @@ IDV.UploadForm = (function() {
 
   function checkUploadSupport() {
     if ($('#files').disabled || navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+      return false;
+    }
+    return true;
+  }
+
+  function checkMultipleUploadSupport() {
+    if (!checkUploadSupport()) {
       return false;
     }
     if (jQuery.browser.mobile) {
@@ -376,23 +393,23 @@ IDV.UploadForm = (function() {
     // Change multiple input behaviour
     useMultiple = false;
     const label = $('.btn-file-upload');
-    const new_label = label.clone();
+    const newLabel = label.clone();
     label.attr('for', 'files-1');
     const input = $('#files');
     input.removeAttr('multiple');
+    input.removeAttr('required');
     input.attr('id', 'files-1');
     const button = document.createElement("input");
     button.type = 'button';
     button.value = 'Add more files';
     button.onclick = function() {
-      fileInputs++;
-      new_label.attr('for', 'files-' + fileInputs);
+      fileInputsCount++;
+      newLabel.attr('for', 'files-' + fileInputsCount);
       const fileInput = document.createElement("input");
       fileInput.type = 'file';
       fileInput.name = 'files';
-      fileInput.id = 'files-' + fileInputs;
-      fileInput.setAttribute('required', 'required');
-      $('input[type=file]:last').after(fileInput).after(new_label.clone()).after('<br/>');
+      fileInput.id = 'files-' + fileInputsCount;
+      $('input[type=file]:last').after(fileInput).after(newLabel.clone()).after('<br/>');
       initImageThumbnails();
     }
     input.after(button).after('<br/>');
@@ -405,7 +422,7 @@ IDV.UploadForm = (function() {
     $form = $('#'+formID);
     $form.submit(submitHandler)
 
-    const uploadSupport = checkUploadSupport();
+    const uploadSupport = checkMultipleUploadSupport();
     if (!uploadSupport) {
       showUploadNotSupportedMessage();
     } else if (uploadSupport === 'partial') {
