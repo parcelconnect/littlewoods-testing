26c079771501200f42ac39b24eeb66103b640c2c
Brian Hernandez <brianhernandez@scurri.com>
2019-02-21 14:33:54 +0000

    Match docker/circleci Python version to Heroku Script to install Python 3.6.8 added Commands for python and pip default to python3.6 and pip3 Dockerfile and circleci updated to use the standard commands Psycopg requirement updated per warning

---
+++ bin/install-python3.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+echo "Installing python3"
+apt-get update && apt-get install -y build-essential checkinstall
+apt-get update && apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
+tar xzf Python-3.6.8.tgz
+pushd Python-3.6.8
+./configure
+make
+make install
+popd
+
+apt-get update && apt-get install -y python3-pip
+
+echo "Making Python3 and Pip3 defaults"
+rm -f /usr/bin/python && ln -s /usr/local/bin/python3 /usr/bin/python
+rm -f /usr/bin/pip && ln -s /usr/local/bin/pip3 /usr/bin/pip
+
+PYTHON_VERSION=$(python --version 2>&1)
+EXPECTED_VERSION="3.6.8"
+if [[ $PYTHON_VERSION != *$EXPECTED_VERSION* ]]; then
+  echo "Python version $PYTHON_VERSION does not match the $EXPECTED_VERSION"
+  exit 1
+fi
+
+echo "Python version verified: $EXPECTED_VERSION"
