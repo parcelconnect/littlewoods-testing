3efe4a60bac9e58a8ce22489966622f0b2dcbc85
Brian Hernandez <brianhernandez@scurri.com>
2019-03-20 17:09:19 +0000

    Use PPA instead of manually installing Python 3.6.8 Python3 installation updated to use PPA

--- bin/install-python3.sh
+++ bin/install-python3.sh
@@ -4,22 +4,17 @@ set -e
 set -x
 
 echo "Installing python3"
-apt-get update && apt-get install -y build-essential checkinstall
+apt-get update && apt-get install -y build-essential checkinstall software-properties-common
 apt-get update && apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
-tar xzf Python-3.6.8.tgz
-pushd Python-3.6.8
-./configure
-make
-make install
-popd
+add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
+apt-get update && apt-get install -y python3.6
 
 apt-get update && apt-get install -y python3-pip
 
 echo "Making Python3 and Pip3 defaults"
-rm -f /usr/bin/python && ln -s /usr/local/bin/python3 /usr/bin/python
-rm -f /usr/bin/pip && ln -s /usr/local/bin/pip3 /usr/bin/pip
+rm -f /usr/bin/python && ln -s /usr/bin/python3 /usr/bin/python
+rm -f /usr/bin/pip && ln -s /usr/bin/pip3 /usr/bin/pip
 
 PYTHON_VERSION=$(python --version 2>&1)
 EXPECTED_VERSION="3.6.8"
